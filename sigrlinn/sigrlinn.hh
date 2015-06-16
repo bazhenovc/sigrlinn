eaa61a006297376b092aead7c65d7e7d6f2b1b21
Kirill Bazhenov <bazhenovc@gmail.com>
2015-06-16 17:55:32 +0300

    Initial Direct3D12 code.

--- sigrlinn/sigrlinn.hh
+++ sigrlinn/sigrlinn.hh
@@ -424,6 +424,7 @@ typedef void(*ErrorReportFunc)(const char*);
 
 //=============================================================================
 bool initD3D11(void* d3dDevice, void* d3dContext, void* d3dSwapChain);
+bool initD3D12(void* d3dDevice);
 bool initOpenGL();
 #ifdef NDA_CODE_AMD_MANTLE
 // NDACodeStripper v0.17: 1 line removed
